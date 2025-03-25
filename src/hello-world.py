ca7f868f8a9ef03ecbf230325a9a78a9dfc698de
Bhavinisah03 <128901410+Bhavinisah03@users.noreply.github.com>
2025-03-25 05:28:25 -0700

    Delete src directory

--- src/hello-world.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
-
-# To run the app, use the command: uvicorn filename:app --reload
